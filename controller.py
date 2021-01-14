dd22d1439546e2f6864d31eca00a82e99a9a8935
siekiera-a <siekiera.a31@gmail.com>
2021-01-14 22:10:12 +0100

    submit endpoint

--- controller.py
+++ controller.py
@@ -8,6 +8,8 @@ from models.score import Score
 from schemas.quiz import quiz_schema
 from schemas.answer import answer_schema
 
+from app import db
+
 controller = Blueprint('controller', __name__)
 
 
@@ -71,7 +73,51 @@ def get_question(question_id):
 @controller.route('/submit/<quiz_id>', methods=['POST'])
 @expects_json(answer_schema)
 def submit_answers(quiz_id):
-    pass
+    quiz_id = parse_id(quiz_id)
+
+    if quiz_id is None:
+        return response_message({'message': 'Invalid quiz id!'}, 400)
+
+    quiz = Quiz.query.filter_by(id=quiz_id).first()
+
+    if quiz is None:
+        return response_message({'message', f'Quiz with id {quiz_id} not found!'}, 404)
+
+    questions_with_answers = []
+
+    for question in quiz.questions:
+        # sort correct answer's ids
+        correct_answers = sorted([a.id for a in question.answers if a.correct is True])
+        questions_with_answers = [*questions_with_answers, {'question': question.id, 'answers': correct_answers}]
+
+    data = g.data
+    points = 0
+
+    # sort request answer's ids
+    for d in data['data']:
+        d['answers'] = sorted(d['answers'])
+
+    # check answers
+    for q in questions_with_answers:
+        if q in data['data']:
+            points += 1
+
+    questions_count = len(quiz.questions)
+    score = points / questions_count
+
+    # save score to db
+    s = Score(quiz_id=quiz.id, user=data['user'], score=score)
+    db.session.add(s)
+    db.session.commit()
+
+    response = {
+        'user': data['user'],
+        'score': points,
+        'total': questions_count,
+        'scoreInPercent': score * 100
+    }
+
+    return jsonify(response)
 
 
 @controller.route('/leaderboard/<quiz_id>')
@@ -95,8 +141,8 @@ def get_leaderboard(quiz_id):
     if limit_arg is not None:
         limit = max(int_or_else_get(limit_arg, other=default_limit), 1)
 
+    # get leaderboard
     scores = [{'user': s.user, 'score': s.score, 'datetime': s.date} for s in quiz.scores]
-
     scores = list(sorted(scores, key=lambda s: s['score'], reverse=True))
     scores = scores[:limit]
 
