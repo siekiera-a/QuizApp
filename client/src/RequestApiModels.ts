87c6c4771a92d484a042bb48bab58c11693bb635
siekiera-a <siekiera.a31@gmail.com>
2021-01-15 21:07:42 +0100

    api models

---
+++ client/src/RequestApiModels.ts
@@ -0,0 +1,25 @@
+export interface IQuestionData {
+  question: string,
+  answers: {
+    text: string,
+    correct: boolean
+  }[]
+}
+
+export interface IQuizRequest {
+  author: string,
+  description: string,
+  data: IQuestionData[]
+}
+
+export interface IAnswerData {
+  question: number,
+  answers: number[]
+}
+
+export interface IAnswerRequest {
+  user: string,
+  data: IAnswerData
+}
+
+export { }
---
+++ client/src/ResponseApiModels.ts
@@ -0,0 +1,63 @@
+// response
+export interface IQuiz {
+  id: number,
+  code: string,
+  author: string,
+  creationTime: string,
+  description: string
+}
+
+export interface IGetQuiz {
+  hasNext: boolean,
+  hasPrev: boolean,
+  page: number,
+  pages: number,
+  total: number,
+  items: IQuiz[]
+}
+
+export interface ICode {
+  code: string
+}
+
+export interface ICode {
+  id: number
+}
+
+export interface IAnswer {
+  id: number,
+  text: string
+}
+
+export interface IQuestion {
+  id: number,
+  text: string,
+  answers: IAnswer[]
+}
+
+export interface ISubmitResponse {
+  user: string,
+  score: number,
+  total: number,
+  scoreInPercent: number
+}
+
+export interface IScore {
+  user: string,
+  score: number,
+  datetime: string
+}
+
+export interface ILeaderboard {
+  author: string,
+  code: string,
+  description: string,
+  creationTime: string,
+  score: IScore[]
+}
+
+export interface IError {
+  message: string
+}
+
+export { }
